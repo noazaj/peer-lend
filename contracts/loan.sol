8e46b55a75fe8db6be2b329945107ff2322969a4
noazaj <noahzajicek@gmail.com>
2024-09-16 21:44:54 -0400

    Create lender functionality for responding to borrow request

--- contracts/loan.sol
+++ contracts/loan.sol
@@ -14,14 +14,44 @@ contract Loan {
 
     struct BorrowerRequest {
         uint256 requestAmount;
-        string description;
+        string  description;
         address requestAddress;
     }
 
-    BorrowerRequest[] public listOfRequests; 
-
-    function borrowerLoanRequest(uint256 _loanAmount, string memory _description) public {
-        listOfRequests.push(BorrowerRequest(_loanAmount, _description, msg.sender));
+    struct LenderRequest {
+        uint256 requestInterest;
+        string  reply;
+        uint256 loanDuration;
+        address acceptingAddress;
     }
 
+    BorrowerRequest[] public listOfBorrowRequests; 
+    LenderRequest[] public listOfLenderRequests;
+
+    function borrowerLoanRequest(uint256 _loanAmount, string memory _description) public validLoanAmount(_loanAmount) {
+        listOfBorrowRequests.push(BorrowerRequest(_loanAmount, _description, msg.sender));
+    }
+
+    function lenderLoanRequest(uint256 _interestRate, string memory _reply, uint256 _loanDuration) public validInterestRate(_interestRate) validLoanDuration(_loanDuration) {
+        bytes memory b = bytes(_reply);
+        if (b.length == 0) {
+            _reply = "";
+        }
+        listOfLenderRequests.push(LenderRequest(_interestRate, _reply, _loanDuration, msg.sender));
+    }
+
+    modifier validLoanAmount(uint256 _loanAmount) {
+        require(_loanAmount > 0, "Need to send amount greater than 0.");
+        _;
+    }
+
+    modifier validInterestRate(uint256 _interestRate) {
+        require(_interestRate > 0, "Need to send rate greater than 0.");
+        _;
+    }
+
+    modifier validLoanDuration(uint256 _loanDuration) {
+        require(_loanDuration > 0, "Loan needs to be longer than 0 months.");
+        _;
+    }
 }
