e3e72ce07e7366741099fda1c2407c1ad795d890
Noah Zajicek <nzajicek@WVWAUTHORITY.COM>
2024-10-01 14:49:55 -0400

    Add mapping to loanDetails and place checks for agreements

--- contracts/loan.sol
+++ contracts/loan.sol
@@ -10,6 +10,7 @@ contract Loan {
         uint256 borrowerRating;
         address borrowerAddress;
         uint256 loanDuration;
+        bool    exists;
     }
 
     struct BorrowerRequest {
@@ -25,19 +26,48 @@ contract Loan {
         address acceptingAddress;
     }
 
-    BorrowerRequest[] public listOfBorrowRequests; 
-    LenderRequest[] public listOfLenderRequests;
+    BorrowerRequest[]   public listOfBorrowRequests; 
+    LenderRequest[]     public listOfLenderRequests;
+
+    mapping(uint256 => LoanDetails) public loanAgreements;
 
     function borrowerLoanRequest(uint256 _loanAmount, string memory _description) public validLoanAmount(_loanAmount) {
         listOfBorrowRequests.push(BorrowerRequest(_loanAmount, _description, msg.sender));
     }
 
-    function lenderLoanRequest(uint256 _interestRate, string memory _reply, uint256 _loanDuration) public validInterestRate(_interestRate) validLoanDuration(_loanDuration) {
+    function lenderLoanRequest(uint256 _interestRate, string memory _reply, uint256 _loanDuration, uint256 _requestIndex) public validInterestRate(_interestRate) validLoanDuration(_loanDuration) {
         bytes memory b = bytes(_reply);
         if (b.length == 0) {
             _reply = "";
         }
-        listOfLenderRequests.push(LenderRequest(_interestRate, _reply, _loanDuration, msg.sender));
+
+        require(_requestIndex < listOfBorrowRequests.length, "Index not within borrower array.");
+
+        // Add lender's proposal
+        LenderRequest memory lenderRequest = LenderRequest(_interestRate, _reply, _loanDuration, msg.sender);
+        listOfLenderRequests.push(lenderRequest);
+
+        // Fetch borrow request
+        BorrowerRequest memory borrowerRequest = listOfBorrowRequests[_requestIndex];
+
+        // Check if requestIndex is already a loanAgreement
+        require(!loanAgreements[_requestIndex].exists, "A loan already exists with specified index.");
+
+        // Map both to LoanDetails
+        mapLoanDetails(_requestIndex, borrowerRequest, lenderRequest);
+    }
+
+    function mapLoanDetails(uint256 _requestIndex, BorrowerRequest memory _borrowRequest, LenderRequest memory _lenderRequest) internal {
+        loanAgreements[_requestIndex] = LoanDetails({
+            amount:             _borrowRequest.requestAmount,
+            interestRate:       _lenderRequest.requestInterest,
+            lenderRating:       0,
+            lenderAddress:      _lenderRequest.acceptingAddress,
+            borrowerRating:     0,
+            borrowerAddress:    _borrowRequest.requestAddress,
+            loanDuration:       _lenderRequest.loanDuration,
+            exists:             true
+        });
     }
 
     modifier validLoanAmount(uint256 _loanAmount) {
